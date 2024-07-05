1c58634ac16068baa535c10446cc4893b03f98d3
ICHRAK <ichrak.ouhibi@esprit.tn>
2024-07-06 00:46:45 +0100

    Gatway commit

---
+++ src/test/java/com/example/getwayapiexamen/GetwayApiExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getwayapiexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwayApiExamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
